50735f4816b6d38b63e069f4e0a23f3ad9d965af
Kotaku432 <njohnso3@nebrwesleyan.edu>
2014-12-05 16:37:11 -0500

    fixed door try 3

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function key_pressed_down(event) {
     	keys += 1;
     	document.getElementById("keys").innerHTML = keys+" keys";
     	key_count = 1;
-    	door_place.element = open_door;
+    	occupants[protagonist.y][protagonist.x] = open_door;
     	render();
     }
     if ( protagonist.x === gem_place.x && protagonist.y === gem_place.y && gem_count===0){

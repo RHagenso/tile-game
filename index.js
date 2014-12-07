d43dd232c8198091a53cfafe27557b278b694895
RHagenso <rhagenso@nebrwesleyan.edu>
2014-12-07 17:43:20 -0500

    Checked off what is done, added GameOver

--- index.js
+++ index.js
@@ -56,6 +56,9 @@ function key_pressed_down(event) {
     	document.getElementById("hearts").innerHTML = hearts+" hearts";
     	heart_count = 1;
     }
+    if ( protagonist.x === enemy_1_place.x && protagonist.y === enemy_1_place.y && gem_count===0){
+    	document.getElementById("GameOver").innerHTML = "GAME OVER";
+    }
     occupants[protagonist.y][protagonist.x] = protagonist.element;
     render();
     

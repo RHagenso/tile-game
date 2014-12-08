7bd96ba44a1e168016819fc3d930bb94dc8bbc8f
RHagenso <rhagenso@nebrwesleyan.edu>
2014-12-07 21:58:07 -0500

    Remove items from playfield when picked up

--- index.js
+++ index.js
@@ -43,16 +43,17 @@ function key_pressed_down(event) {
     if ( protagonist.x === key_place.x && protagonist.y === key_place.y && key_count===0) {
     	key_count += 1;
     	document.getElementById("keys").innerHTML = key_count+" keys";
-    	//commented out for testing: occupants[door_place.y][door_place.x] = open_door;
-    	//render();
+    	key_place.x = undefined;
     }
     if ( protagonist.x === gem_place.x && protagonist.y === gem_place.y){
     	gem_count += 1;
     	document.getElementById("gems").innerHTML = gem_count+" gems";
+    	gem_place.x = undefined;
     }
     if ( protagonist.x === heart_place.x && protagonist.y === heart_place.y){
     	heart_count += 1;
     	document.getElementById("hearts").innerHTML = heart_count+" hearts";
+    	heart_place.x = undefined;
     }
     if ( protagonist.x === star_place.x && protagonist.y === star_place.y){
     	heart_count = heart_count*2;
@@ -61,6 +62,7 @@ function key_pressed_down(event) {
     	document.getElementById("gems").innerHTML = gem_count+" gems";
     	key_count = key_count*2;
     	document.getElementById("keys").innerHTML = key_count+" keys";
+    	star_place.x = undefined;
     }
     //Game over conditions
     if ( protagonist.x === enemy_1_place.x && protagonist.y === enemy_1_place.y && gem_count===0){

8d356dfcb7360656a12ab0d6ed782b136a09ddfc
RHagenso <rhagenso@nebrwesleyan.edu>
2014-12-07 21:37:49 -0500

    Checked off gem defeats enemy, added star usage, require walk over door to unlock

--- index.js
+++ index.js
@@ -39,11 +39,12 @@ function key_pressed_down(event) {
             protagonist.y = protagonist.y + 1;
         }
     }
+    //Inventory conditions
     if ( protagonist.x === key_place.x && protagonist.y === key_place.y && key_count===0) {
     	key_count += 1;
     	document.getElementById("keys").innerHTML = key_count+" keys";
-    	occupants[door_place.y][door_place.x] = open_door;
-    	render();
+    	//commented out for testing: occupants[door_place.y][door_place.x] = open_door;
+    	//render();
     }
     if ( protagonist.x === gem_place.x && protagonist.y === gem_place.y){
     	gem_count += 1;
@@ -52,6 +53,14 @@ function key_pressed_down(event) {
     if ( protagonist.x === heart_place.x && protagonist.y === heart_place.y){
     	heart_count += 1;
     	document.getElementById("hearts").innerHTML = heart_count+" hearts";
+    }
+    if ( protagonist.x === star_place.x && protagonist.y === star_place.y){
+    	heart_count = heart_count*2;
+    	document.getElementById("hearts").innerHTML = heart_count+" hearts";
+    	gem_count = gem_count*2;
+    	document.getElementById("gems").innerHTML = gem_count+" gems";
+    	key_count = key_count*2;
+    	document.getElementById("keys").innerHTML = key_count+" keys";
     }
     //Game over conditions
     if ( protagonist.x === enemy_1_place.x && protagonist.y === enemy_1_place.y && gem_count===0){
@@ -72,6 +81,15 @@ function key_pressed_down(event) {
     	gem_count -= 1;
     	document.getElementById("gems").innerHTML = gem_count+" gems";
     	enemy_2_place.x = undefined;
+    }
+    //Opening door(s)
+    if (protagonist.x === door_place.x && protagonist.y === door_place.y && key_count>0) {
+       	key_count -= 1;
+    }
+    else {
+    	protagonist.y = 2;
+    	protagonist.x = 2;
+    	window.alert("Not enough keys, try again.")
     }
     occupants[protagonist.y][protagonist.x] = protagonist.element;
     render();

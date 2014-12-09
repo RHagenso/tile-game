43c3c39ff04d1fb1a97d775f23816ac2374167bd
Kotaku432 <njohnso3@nebrwesleyan.edu>
2014-12-08 20:04:40 -0500

    fixed door graphics

--- index.js
+++ index.js
@@ -101,10 +101,14 @@ function key_pressed_down(event) {
        	key_count -= 1;
        	document.getElementById("keys").innerHTML = key_count+" keys";
        	door_place.element = open_door;
-    	door_place.y = -1;
-    	door_place.x = -1;
     }
-    occupants[protagonist.y][protagonist.x] = protagonist.element;
+    if (door_place.x === protagonist.x && door_place.y === protagonist.y){
+    	occupants[protagonist.y][protagonist.x] = protagonist.element, open_door
+    } else if(selector_place.x === protagonist.x && selector_place.y === protagonist.y){
+    	occupants[protagonist.y][protagonist.x] = protagonist.element, selector
+    } else{
+    	occupants[protagonist.y][protagonist.x] = protagonist.element;
+    }
     render();
     
 }
